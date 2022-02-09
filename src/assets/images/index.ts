fe933ea94b7ed0ca0a9a1b16c20b6188089c8d77
Zandré Broodryk <zandre.broodryk@codehesion.co.za>
2022-02-09 20:17:42 +0200

    POC icon that animates on mouse over. -add still gif image and its moving url counterpart (temp for POC) - add poc image that only moves on mouse over on homescreen

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,1 +1,3 @@
 export { default as Logo } from './logo.png';
+// TODO: remove or replace after POC
+export { default as Icon } from './icon.gif';
--- src/screens/app/home/home.screen.tsx
+++ src/screens/app/home/home.screen.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Icon } from '../../../assets';
 
-import { AppLayout } from '../../../components';
 import { userSelector } from '../../../reducers';
 
 export const HomeScreen: React.FC = () => {
   const { user } = useSelector(userSelector);
+  const [imageSource, setImageSource] = useState<string>(
+    Icon,
+  );
 
   return (
-    <AppLayout>
-      <div className="p-5">
-        <p>Welcome, {user.email}</p>
-      </div>
-    </AppLayout>
+    <div className="p-5">
+      <p>Welcome, {user.email}</p>
+      <img
+        src={imageSource}
+        onMouseEnter={() => setImageSource('https://media.giphy.com/media/cvbGraL8NJJZu/giphy.gif')}
+        onMouseLeave={() => setImageSource(Icon)}
+      />
+    </div>
   );
 };
